a772a7770b1476b517ec6875c07948e853e525b9
U-PERSO-22BACE281\bertrand <bertrand@perso-22bace281.(none)>
2009-03-10 12:15:21 +0100

    Removed branched (old svn branch) as it is not needed.

--- branch-1/constants.mm
+++
@@ -1,5 +0,0 @@
-const L = 0;
-const U = 1;
-const R = 2;
-const D = 3;
-const h_names = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
--- branch-1/constants.mm~
+++
@@ -1,4 +0,0 @@
-const L = 0;
-const U = 1;
-const R = 2;
-const D = 3;
